ffac794844f58e8da7538cdfd5b4b1fffc4938d3
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-05-11 15:45:18 -0400

    imported OZ contracts needed

---
+++ src/POAP.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "lib/openzeppelin-contracts/contracts/security/Pausable.sol";
+import "lib/openzeppelin-contracts/contracts/utils/Counters.sol";
+import "lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
+
+contract POAP is ERC721, Pausable, AccessControl {
+ 
+}
