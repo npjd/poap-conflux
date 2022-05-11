6ffa5ae56bee332d6855fc6618d1462b23f0f067
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-05-11 16:14:40 -0400

    added forge std and started poap test contract

--- test/POAP.t.sol
+++ test/POAP.t.sol
@@ -1,6 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
 import "ds-test/test.sol";
 import "../src/POAP.sol";
 
-contract POAPTest is DSTest {
-    
+contract POAPTest is DSTest,POAP {
+
+    function setUp() public {
+        
+    }
+
 }
